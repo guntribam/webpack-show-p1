887dc7e98ac7e7db159042c9f56ac2b866f52c26
guntribam <gunter.amorim@gmail.com>
2018-03-15 02:18:09 -0300

    B4

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,16 @@
 const path = require('path');
 const BabiliPlugin = require('babili-webpack-plugin');
+const webpack = require('webpack')
 
 let plugins = []
 
+let SERVICE_URL = JSON.stringify('http://localhost:3000')
+
 if(process.env.NODE_ENV === "production"){
+	SERVICE_URL = JSON.stringify('http://bobmarley.com')
 	plugins.push(new BabiliPlugin());
 }
+plugins.push(new webpack.DefinePlugin({SERVICE_URL}))
 
 module.exports = {
 	entry: './app-src/app.js',
